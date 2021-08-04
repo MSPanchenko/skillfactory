2540c85caa8f5dcbf8d1b05f8f348c61fc8071ff
mspanchenko <maxpanchnko@gmail.com>
2021-08-04 13:36:32 +0300

    finish final task from module 0

--- 0_project_0-github/main.py
+++ 0_project_0-github/main.py
@@ -1,31 +1,42 @@
 import numpy as np
 
 # версия питона 3.9
+#
+# pep8 гласит: Или же, используйте старое соглашение, добавляя перед именами таких глобальных переменных
+# один символ подчеркивания (которым вы можете обозначить те глобальные переменные,
+# которые используются только внутри модуля).
+#
+# Именно поэтому использовал подчеркивание
 
-from_int = 1
-to_int = 101
+_min_number = 1
+_max_number = 101
 
 
 def game_core_v2(number):
-    # Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем
-    # его в зависимости от того, больше оно или меньше нужного.
-    # Функция принимает загаданное число и возвращает число попыток
     count = 1
-    predict = np.random.randint(from_int, to_int)
+    predict = np.random.randint(_min_number, _max_number)
+    min_number = _min_number
+    max_number = _max_number
     while number != predict:
         count += 1
+
         if number > predict:
-            predict += 1
+            min_number = predict
         elif number < predict:
-            predict -= 1
-    return count  # выход из цикла, если угадали
+            max_number = predict
+
+        diff = max_number - min_number
+        diff = 1 if diff == 1 else int(diff / 2)
+
+        predict = (min_number + diff if number > predict else max_number - diff)
+
+    return count
 
 
 def score_game(game_core):
-    # Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число
     count_ls = []
-    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
-    random_array = np.random.randint(from_int, to_int, size=1000)
+    np.random.seed(1)
+    random_array = np.random.randint(_min_number, _max_number, size=10000)
     for number in random_array:
         count_ls.append(game_core(number))
     score = int(np.mean(count_ls))
