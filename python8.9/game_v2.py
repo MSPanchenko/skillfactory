8c8d31b809a9502ba33609afc5d73b489ccbaa0b
maxpanchnko <maxpanchnko@gmail.com>
2021-11-23 16:14:06 +0200

    add fixed method for finding number

--- python8.9/game_v2.py
+++ python8.9/game_v2.py
@@ -1,7 +1,9 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
 import numpy as np
 
-# версия питона 3.9
-#
 # pep8 гласит: Или же, используйте старое соглашение, добавляя перед именами таких глобальных переменных
 # один символ подчеркивания (которым вы можете обозначить те глобальные переменные,
 # которые используются только внутри модуля).
@@ -11,8 +13,15 @@ import numpy as np
 _min_number = 1
 _max_number = 101
 
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
 
-def game_core_v2(number):
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
     count = 1
     predict = np.random.randint(_min_number, _max_number)
     # копируем минимум и максимум для локального редактирования диапазона чисел
@@ -35,15 +44,27 @@ def game_core_v2(number):
     return count
 
 
-def score_game(game_core):
+def score_game(random_predict) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
     count_ls = []
-    np.random.seed(1)
-    random_array = np.random.randint(_min_number, _max_number, size=10000)
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
     for number in random_array:
-        count_ls.append(game_core(number))
+        count_ls.append(random_predict(number))
+
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
     return score
 
 
-score_game(game_core_v2)
+if __name__ == "__main__":
+    # RUN
+    score_game(random_predict)
