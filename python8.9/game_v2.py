8db5ddcfb73221d5f601d0cdaac15c3023d7f93b
maxpanchnko <maxpanchnko@gmail.com>
2021-11-23 16:41:18 +0200

    finish project

--- python8.9/game_v2.py
+++ python8.9/game_v2.py
@@ -55,13 +55,13 @@ def score_game(random_predict) -> int:
     """
     count_ls = []
     #np.random.seed(1)  # фиксируем сид для воспроизводимости
-    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+    random_array = np.random.randint(_min_number, _max_number, size=(1000))  # загадали список чисел
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
     score = int(np.mean(count_ls))
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попыток")
     return score
 
 
